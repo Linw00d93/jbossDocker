4f3690aba7a12e28cdf5f8bd84e51e8a7c72c78b
Linw00d93 <42758858+Linw00d93@users.noreply.github.com>
2021-09-29 20:21:16 -0400

    Update installJava.sh

--- installJava.sh
+++ installJava.sh
@@ -5,7 +5,7 @@ sudo tar -xvzf openjdk-7u75-b13-linux-x64-18_dec_2014.tar.gz && \
 cd /etc/ && \
 sudo rm environment && \
 touch enviroment && \
-cat PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-se-7u75-ri/bin:/usr/lib/jvm/java-se-7u75-ri/db/bin:/usr/lib/jvm/java-se-7u75-ri/jre/bin" J2SDKDIR="/usr/lib/jvm/java-se-7u75-ri" J2REDIR="/usr/lib/jvm/java-se-7u75-ri/jre* JAVA_HOME="/usr/lib/jvm/java-se-7u75-ri" DERBY_HOME="/usr/lib/jvm/java-se-7u75-ri/db" >> enviroment && \
+echo PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-se-7u75-ri/bin:/usr/lib/jvm/java-se-7u75-ri/db/bin:/usr/lib/jvm/java-se-7u75-ri/jre/bin" J2SDKDIR="/usr/lib/jvm/java-se-7u75-ri" J2REDIR="/usr/lib/jvm/java-se-7u75-ri/jre*" JAVA_HOME="/usr/lib/jvm/java-se-7u75-ri" DERBY_HOME="/usr/lib/jvm/java-se-7u75-ri/db" >> enviroment && \
 sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/java-se-7u75-ri/bin/java" 0 && \
 sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/java-se-7u75-ri/bin/javac" 0 && \
 sudo update-alternatives --set java /usr/lib/jvm/java-se-7u75-ri/bin/java && \
